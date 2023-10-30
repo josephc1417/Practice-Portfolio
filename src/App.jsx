cc65e870445075fb362bef6b0d06fb57932ecd66
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-10-30 08:57:23 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar.jsx";
+
+
+const App = () => {
+    return (
+        <di>
+            <h1 className={'text-emerald-600 underline text-center'}>App</h1>
+            <Navbar />
+        </di>
+    )
+}
+export default App
